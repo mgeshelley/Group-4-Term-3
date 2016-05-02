db8b94abbc593b619ade9168871ced062e992f3a
Matthew Shelley <mges501@york.ac.uk>
2016-05-02 14:21:44 +0100

    Commit

--- git_sync.sh
+++ git_sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-git pull
+# Script to sync local directory with Github repository
+
 git add .
 git commit -m "Commit"
+git pull 
 # git remote add origin git@github.com:mges501York/Group-4-Term-3
 git push origin master
 git pull
