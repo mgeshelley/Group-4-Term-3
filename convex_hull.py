9d7e5877731281900fefe6899322562931877392
Matthew Shelley <mges501@york.ac.uk>
2016-05-02 16:14:40 +0100

    Commit

--- convex_hull.py
+++ convex_hull.py
@@ -10,28 +10,19 @@ parameter being varied. Creates convex hull for data.
 #Read in data
 file_name = 'madeup.dat'
 data = np.genfromtxt(file_name, usecols = (0, 1))
-
 n = len(data)
 
-#Array for hull vertices
-hv = np.zeros((1,2))
-
-#First hull vertex must be first data point
-hv[0,:] = data[0,:]
-
+convex_hull = open('convex_hull.dat', 'w')
+convex_hull.write("%f %f\n" % (data[0,0], data[0,1]))
 hv_new = 0
-theta_max = 4.0 * np.arctan(1.0)
-
-while hv_new != n:
-    points_to_check = n - hv_new
-    for i in range (0, points_to_check):
+while hv_new < n-1:
+    #-pi/2 as minimum possible angle
+    theta_max = -2.0 * np.arctan(1.0)
+    for i in range (hv_new+1, n):
         theta = -1 * np.arctan((data[i,1] - data[hv_new,1])/(data[i,0] - data[hv_new,0]))
-        max_loc = hv_new + i
+        print theta, theta_max
         if theta > theta_max:
             theta_max = theta
-            max_loc = hv_new + i
-    hv = [[hv],[data[max_loc,:]]]
+            max_loc = i
     hv_new = max_loc
-
-#Write 'hv' to file
-convex_hull = open('convex_hull.dat', 'w')
+    convex_hull.write("%f %f\n" % (data[hv_new,0], data[hv_new,1]))
