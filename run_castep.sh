4f1ed11ead4d64a181780d36f7d2556c7d51e5c3
George Watson <spam@dendodge.me>
2016-05-02 15:28:25 +0100

    Choose 'cube' or 'sheet'

--- run_castep.sh
+++ run_castep.sh
@@ -3,10 +3,12 @@
 # Get user input
 read -r -p "Delete old .castep files? [yN] " -n 1 yn
 echo ""
+read -r -p "Enter 'cube' or 'sheet': " latticetype
+echo ""
 
 ./lattice.exe
 
-for name in $( ls *.cell | sed 's/\(.*\)\..*/\1/' ); do
+for name in $( ls $latticetype*.cell | sed 's/\(.*\)\..*/\1/' ); do
     # Delete the old .castep file if yn is yes; default is to keep
     case $yn in
         [Yy]* ) rm "$name.castep";;
