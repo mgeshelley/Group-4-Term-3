0d1b023de07738945692a2b84c6fb5b3d820a58e
George Watson <spam@dendodge.me>
2016-05-02 15:47:21 +0100

    Minor shell script fixes
    * Removed superfluous blank echos
    * Added -f option to rm command

--- run_castep.sh
+++ run_castep.sh
@@ -4,16 +4,14 @@
 read -r -p "Delete old .castep files? [yN] " -n 1 yn
 echo ""
 read -r -p "Enter 'cube' or 'sheet': " latticetype
-echo ""
 read -r -p "How many cores? " numcores
-echo ""
 
 ./lattice.exe
 
 for name in $( ls $latticetype*.cell | sed 's/\(.*\)\..*/\1/' ); do
     # Delete the old .castep file if yn is yes; default is to keep
     case $yn in
-        [Yy]* ) rm "$name.castep";;
+        [Yy]* ) rm -f "$name.castep";;
         * ) ;;
     esac
 
