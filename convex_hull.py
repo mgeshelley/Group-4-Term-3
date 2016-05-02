a3ec10bb502c0c6a574cb2f8fa8a37dcef140dc4
Matthew Shelley <mges501@york.ac.uk>
2016-05-02 15:09:20 +0100

    Commit

--- convex_hull.py
+++ convex_hull.py
@@ -8,8 +8,10 @@ parameter being varied. Creates convex hull for data.
 """
 
 #Read in data
-file_name = raw_input("Enter name of file containing energies")
+file_name = 'madeup.dat'
 data = np.genfromtxt(file_name, usecols = (0, 1))
+
+n = len(data)
 
 #Array for hull vertices
 hv = np.zeros((1,2))
@@ -18,8 +20,18 @@ hv = np.zeros((1,2))
 hv[0,:] = data[0,:]
 
 hv_new = 0
-theta_max = -1 * np.atan((data[1,1] - data[0,1])/(data[1,0] - data[0,0]))
-while
+theta_max = 4.0 * np.arctan(1.0)
+
+while hv_new != n:
+    points_to_check = n - hv_new
+    for i in range (0, points_to_check):
+        theta = -1 * np.arctan((data[i,1] - data[hv_new,1])/(data[i,0] - data[hv_new,0]))
+        max_loc = hv_new + i
+        if theta > theta_max:
+            theta_max = theta
+            max_loc = hv_new + i
+    hv = [[hv],[data[max_loc,:]]]
+    hv_new = max_loc
 
 #Write 'hv' to file
 convex_hull = open('convex_hull.dat', 'w')
