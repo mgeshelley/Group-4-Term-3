2e7deae2785f3f86d421bad683d436cbcf3fc433
George Watson <spam@georgewatson.me>
2016-05-02 14:41:54 +0100

    Loop over files

--- run_castep.sh
+++ run_castep.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Get user input
-read -r -p "Cell name: " name
-read -r -p "Delete old .castep file? [Yn] " -n 1 yn
+read -r -p "Delete old .castep files? [Yn] " -n 1 yn
+echo ""
 
-# Delete the old .castep file if yn is yes; default is to delete
-case $yn in
-    [Yy]* ) rm "$name.castep";;
-    * ) ;;
-esac
+for name in $( ls *.cell | sed 's/\(.*\)\..*/\1/' ); do
+    # Delete the old .castep file if yn is yes; default is to keep
+    case $yn in
+        [Yy]* ) rm "$name.castep";;
+        * ) ;;
+    esac
 
-# Run CASTEP
-mpirun -np 3 castep.mpi $name &
+    # Run CASTEP
+    mpirun -np 3 castep.mpi $name &
 
-# Get energies from the output file
-grep 'total energy' "$name.castep"
+    # Get energies from the output file
+    grep 'total energy' "$name.castep"
+done
