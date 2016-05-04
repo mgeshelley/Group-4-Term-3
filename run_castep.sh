20e5c366f240cb93b8d57a494f8a4dbb9673f39f
George Watson <spam@dendodge.me>
2016-05-04 12:38:50 +0100

    Make the cluster actually work

--- run_castep.sh
+++ run_castep.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-# Get user input
-#read -r -p "Delete old .castep files? [yN] " -n 1 yn
-#echo ""
-#read -r -p "Enter 'cube' or 'sheet': " latticetype
-#read -r -p "How many cores? " numcores
+#This is a generic script to execute a parallel command on jorvik using Sun Grid Engine
+#Lines beginning with one "#" are comment lines and ignored
+#Lines beginning with "#$" are instructions to the qsub command
+#Ebor has 32 cores per node 
+#v1.0 MIJP 20/11/2015
+
+#specify export QSUB vars to shell script
+#$ -V -j y -R y 
+
+#specify which queue (phys-teaching = high priority, short max time; phys-cluster = low priority, longer max time) 
+#$ -q phys-cluster
+
+#execute script from current working directory 
+#$ -cwd
+
+#select max run-time
+#$ -l h_rt=00:05:00
+
+#select parallel environment to run on nn cores, max 32 cores/node
+#$ -pe mpi-16 32
+#set same value here too
+NUM_CORES=32
+
+#name of MPI executable
+EXEC=castep.mpi
+
+#any additional arguments to pass to the executable
+ARGS=$1
+
+#set job running
+
 
 yn="n"
 latticetype="cube"
@@ -24,7 +50,7 @@ for name in $( ls $latticetype*.cell | sed 's/\(.*\)\..*/\1/' ); do
     cp base.param $name.param
 
     # Run CASTEP
-    ./run_castep_ebor.sh $name
+    mpirun -np $NUM_CORES $EXEC $name
 
     # Get energies from the output file
     grep 'total energy' "$name.castep"
